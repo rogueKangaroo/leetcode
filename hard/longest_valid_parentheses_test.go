5f6d5f97ddc8c055994af537d4b28a270357a378
liukaizhao <liukaizhao@bytedance.com>
2019-05-13 15:52:04 +0800

    动态规划longest_valid_parentheses

--- hard/longest_valid_parentheses_test.go
+++ hard/longest_valid_parentheses_test.go
@@ -7,28 +7,28 @@ import (
 )
 
 func Test_longestValidParentheses(t *testing.T) {
-	//s := "()()()()"
-	//got := longestValidParentheses(s)
-	//fmt.Println(fmt.Sprintf("longestValidParentheses result is %v", got))
-	//time.Sleep(100 * time.Millisecond)
-	//
-	//s = "(()"
-	//got = longestValidParentheses(s)
-	//fmt.Println(fmt.Sprintf("longestValidParentheses result is %v", got))
-	//time.Sleep(100 * time.Millisecond)
-	//
-	//s = ")()())"
-	//got = longestValidParentheses(s)
-	//fmt.Println(fmt.Sprintf("longestValidParentheses result is %v", got))
-	//time.Sleep(100 * time.Millisecond)
-	//
-	//s = "()(()"
-	//got = longestValidParentheses(s)
-	//fmt.Println(fmt.Sprintf("longestValidParentheses result is %v", got))
-	//time.Sleep(100 * time.Millisecond)
-
-	s := "(()())"
+	s := "()()()()"
 	got := longestValidParentheses(s)
+	fmt.Println(fmt.Sprintf("longestValidParentheses result is %v", got))
+	time.Sleep(100 * time.Millisecond)
+
+	s = "(()"
+	got = longestValidParentheses(s)
+	fmt.Println(fmt.Sprintf("longestValidParentheses result is %v", got))
+	time.Sleep(100 * time.Millisecond)
+
+	s = ")()())"
+	got = longestValidParentheses(s)
+	fmt.Println(fmt.Sprintf("longestValidParentheses result is %v", got))
+	time.Sleep(100 * time.Millisecond)
+
+	s = "()(()"
+	got = longestValidParentheses(s)
+	fmt.Println(fmt.Sprintf("longestValidParentheses result is %v", got))
+	time.Sleep(100 * time.Millisecond)
+
+	s = "(()())"
+	got = longestValidParentheses(s)
 	fmt.Println(fmt.Sprintf("longestValidParentheses result is %v", got))
 	time.Sleep(100 * time.Millisecond)
 }
